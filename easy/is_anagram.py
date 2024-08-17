cb5988a59d41b67860b945a21b769bac0d9b8be7
Deep Dive Security <165413290+deepdivesecurity@users.noreply.github.com>
2024-08-17 19:54:30 -0400

    Update is_anagram.py

--- easy/is_anagram.py
+++ easy/is_anagram.py
@@ -3,11 +3,11 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # Get the character counts as a hashtable
-        sCount = Counter(s)
-        tCount = Counter(t)
+        s_count = Counter(s)
+        t_count = Counter(t)
 
         # Check if the hashtables are equal
-        if sCount == tCount: 
+        if s_count == t_count: 
             return True
         else: 
             return False
