26b1bc86264db9f1495a22fbf4d6533ccf110bb8
Deep Dive Security <165413290+deepdivesecurity@users.noreply.github.com>
2024-08-17 19:54:57 -0400

    Update duplicate_integer.py

--- easy/duplicate_integer.py
+++ easy/duplicate_integer.py
@@ -1,10 +1,10 @@
 class Solution:
   def hasDuplicate(self, nums: List[int]) -> bool:
     # Create a set based off of the nums list argument because sets can't contain duplicates
-    numSet = set(nums)
+    num_set = set(nums)
     
     # Check if the length of the set is different than the length of the list argument
-    if len(numSet) != len(nums): 
+    if len(num_set) != len(nums): 
       # If the lengths aren't the same, a duplicate was present
       return True
     else: 
